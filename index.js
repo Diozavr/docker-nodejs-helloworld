166645c07540c80598588280d83e8a48ed4767ff
Мигулин Денис Михайлович <dio@cmx.ru>
2017-06-29 16:34:41 +0300

    Signed-off-by: Мигулин Денис Михайлович <dio@cmx.ru>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 
 // App
 var app = express();
-app.get('/', function (req, res) {
+app.get('/nodejs-example/', function (req, res) {
   res.send('Hello World\n');
 });
 
